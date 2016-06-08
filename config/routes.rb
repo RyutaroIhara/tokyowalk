2a91e9f2c34ce7a72c34398608a543ebb4b75513
RyutaroIhara <gate8129@gmail.com>
2016-06-09 01:15:47 +0900

    walkモデルとreviewモデルのroutingをネストさせた。

--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,16 @@
 Rails.application.routes.draw do
   devise_for :users
-  get 'users/:id' => 'users#show'
-  get 'walks/:id' => 'walks#show'
   root 'walks#index'
-  get 'walks' => 'walks#index'
-  get 'walks/new' => 'walks#new'
-  post 'walks' => 'walks#create'
-  delete 'walks/:id' => 'walks#destroy'
-  get 'walks/:id/edit' => 'walks#edit'
-  patch 'walks/:id' => 'walks#update'
+  # get 'users/:id' => 'users#show'
+  # get 'walks/:id' => 'walks#show'
+  # get 'walks' => 'walks#index'
+  # get 'walks/new' => 'walks#new'
+  # post 'walks' => 'walks#create'
+  # delete 'walks/:id' => 'walks#destroy'
+  # get 'walks/:id/edit' => 'walks#edit'
+  # patch 'walks/:id' => 'walks#update'
+  resources :walks do
+    resources :reviews, only: [:create]
+  end
+  resources :users, only: [:show]
 end
